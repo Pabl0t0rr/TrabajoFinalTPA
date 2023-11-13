fbac616b51390dea9c770516841b40bfa031b6bc
Pabl0t0rr <pablotorr04@gmail.com>
2023-11-13 22:05:57 +0100

    Javadocs,clases nuevas

--- src/tiendaOnline/Pieza.java
+++ src/tiendaOnline/Pieza.java
@@ -1,6 +1,18 @@
 package tiendaOnline;
 
+/**
+ * La clase Pieza representa un producto de tipo pieza en la tienda online.
+ * Extiende la clase Producto y hereda sus atributos y métodos.
+ */
 class Pieza extends Producto {
+    /**
+     * Constructor para la clase Pieza.
+     *
+     * @param nombre      El nombre de la pieza.
+     * @param precio      El precio de la pieza.
+     * @param stock       La cantidad en stock de la pieza.
+     * @param descripcion Una breve descripción de la pieza.
+     */
     public Pieza(String nombre, double precio, int stock, String descripcion) {
         super(nombre, precio, stock, descripcion, "Pieza");
     }
