6e3976a8d7810c41ca705e69cec8b4521f1da625
Pabl0t0rr <pablotorr04@gmail.com>
2023-11-21 18:12:41 +0100

    code style

--- src/tiendaOnline/TestConexion.java
+++ src/tiendaOnline/TestConexion.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class TestCOnexion {
+public class TestConexion {
 	public static void main(String[] args) {
 		Conexion conexion = new Conexion();
 		Connection cn = null;
